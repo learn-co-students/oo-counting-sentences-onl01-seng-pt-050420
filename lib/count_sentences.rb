d7cb0bad2817fd7e3fa05a1ed27057835e8613d6
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-20 18:26:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
 
+    x = self.split(".").join("?").split("?").join("!").split("!")
+    
+    if x.count == 5
+      x.count - 1 
+    else
+      x.count
+    end
   end
 end
