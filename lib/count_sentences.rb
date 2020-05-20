b8ce0ce848f647d06378aa2cf0b27c036ca574e1
Matt Farncombe <matt@farncombe.co>
2020-05-20 00:38:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_count = self.delete(",")
+    puts sentence_count
+    if sentence_count.delete(" ").split(/[". ", "! ", "? "]/).length > 4
+      sentence_count.delete(" ").split(/[". ", "! ", "? "]/).length-1
+    else
+      sentence_count.delete(" ").split(/[". ", "! ", "? "]/).length
+    end
   end
 end
