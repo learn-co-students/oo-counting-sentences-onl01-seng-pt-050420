0408b3c4051b6d77a4823f811931b92dfda25842
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-22 21:52:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+    return true
+    else false
+   end
   end
+
 
   def question?
-
+   if self.end_with?("?")
+    return true
+    else false
+    end
   end
+
 
   def exclamation?
-
+    if self.end_with?("!")
+    return true
+   else false
+   end
   end
 
-  def count_sentences
 
+  def count_sentences
+    sentence_array = self.split(/[.?!]+/).reject {|string| string.empty?}
+    return sentence_array.count
   end
 end
