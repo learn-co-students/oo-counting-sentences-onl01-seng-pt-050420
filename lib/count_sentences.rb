c4971df861908dc74946c9b5de9ba6a401fbef31
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-01 04:26:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,18 +1,18 @@
 class String
 
   def sentence?
-    self.end_with(".") 
+    self.end_with?(".") ? true : false
   end
 
   def question?
-    self.end_with("?") 
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-    self.end_with("!") 
+    self.end_with?("!") ? true : false
   end
 
-   def count_sentences
-    self.scan(/[.?!]+/).length
+  def count_sentences
+    self.split(/[.?!]+/).count
   end
 end
